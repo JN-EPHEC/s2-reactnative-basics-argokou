262827561b5a6a34da3db42c47d6260709d7fcd7
Your Name <you@example.com>
2025-10-07 17:39:26 +0200

    Homework Assignment S2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,54 +1,29 @@
-import ProfileCard from "@/components/ProfileCard";
-import { ScrollView, StyleSheet } from "react-native";
-
-const USERS_DATA = [
-  { id: '1', nom: 'Alice Martin', email: 'alice.martin@email.com' },
-  { id: '2', nom: 'Benjamin Dubois', email: 'ben.dubois@email.com' },
-  { id: '3', nom: 'Chloé Garcia', email: 'chloe.g@email.com' },
-  { id: '4', nom: 'David Petit', email: 'david.petit@email.com' },
-  { id: '5', nom: 'Émilie Rousseau', email: 'emilie.rousseau@email.com' },
-];
-
+import { StyleSheet, Text, View } from "react-native";
+import TodoItem from "../../components/TodoItem"; // ici c'est le chemin 
 
 export default function HomeScreen() {
   return (
-      <ScrollView>
-        <ProfileCard
-  name="ALICE"
-  jobTitle="React Native Developer"
-  imageUrl="https://picsum.photos/200/300"
-/>
-      </ScrollView>
+    <View style={styles.container}>
+      {/* Title */}
+      <Text style={styles.title}>My Todo List</Text>
+
+      <TodoItem text="Learn React Native" />
+      <TodoItem text="Build a static UI" />
+      <TodoItem text="Practice with StyleSheet" />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({  // ici je crée pas mage
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
+    padding: 20,
+    backgroundColor: "#8f8f92ff",
   },
   title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginVertical: 20,
-  },
-  item: {
-    backgroundColor: '#ffffff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#ddd',
-  },
-  nom: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  email: {
-    fontSize: 14,
-    color: '#666',
-    marginTop: 4,
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
   },
 });
